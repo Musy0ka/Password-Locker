96f46e11fc03d5e3bfac4dd5b315555cf84418cd
Musy0ka <allan.musyoka@student.moringaschool.com>
2022-05-05 12:52:55 +0300

    added save_user test

--- user_test.py
+++ user_test.py
@@ -16,6 +16,14 @@ class TestUser(unittest.TestCase):
     self.assertEqual(self.new_user.last,"Musyoka")
     self.assertEqual(self.new_user.phone_number,"0729789761")
 
+  # Test to save user
+  def test_save_user(self):
+    '''
+    test_save_user test case to test if the user object is saved into the user list
+    '''
+    self.new_user.save_user() # saving the new user
+    self.assertEqual(len(User.user_list),1)
+
 
 if __name__ == '__main__':
   unittest.main()
