bab646aaf85f849cbceb8727f7fb5a141edca032
Musy0ka <allan.musyoka@student.moringaschool.com>
2022-05-09 10:31:35 +0300

    Added test to display all users saved

--- user_test.py
+++ user_test.py
@@ -68,6 +68,13 @@ class TestUser(unittest.TestCase):
 
     self.assertTrue(user_exists)
 
+  def test_display_all_users(self):
+    '''
+    Method that returns a list of all users saved
+    '''
+
+    self.assertEqual(User.display_users(),User.user_list)
+
 
 if __name__ == '__main__':
   unittest.main()
