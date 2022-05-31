59336a5d865a8d3c72bb9633aff7c71c66f1276b
Musy0ka <allan.musyoka@student.moringaschool.com>
2022-05-31 09:58:25 +0300

    commit another

--- user.py
+++ user.py
@@ -2,7 +2,7 @@ class User:
     '''
     This class helps generate instances for creating a new account on this application
     '''
-    profile_list = [] # empty profile list
+    profile_list = []
     def __init__(self, userName, password):
         '''
         This function helps define properties to create a PASSWORD-MANAGER account.
